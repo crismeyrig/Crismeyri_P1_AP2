aa9d44226dcf30a53956ce8188cff89d86a487e3
Crismeyri Gutierrez Gil <crismeyri_gutierrez@ucne.edu.do>
2021-05-31 14:37:14 -0400

    Agregar archivos de proyecto.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Crismeyri_P1_AP2.Models;
+
+namespace Crismeyri_P1_AP2.DAL
+{
+    public class Contexto : DbContext
+    {
+       
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+    }
+
+}
