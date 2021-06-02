1d7d1b702963c3be862cebbbcc8619eff4085fb7
Crismeyri Gutierrez Gil <crismeyri_gutierrez@ucne.edu.do>
2021-06-02 19:27:44 -0400

    Agregando la Bll y el Contexto

---
+++ BLL/ProductosBLL.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Crismeyri_P1_AP2.Models;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Crismeyri_P1_AP2.DAL;
+
+
+
+namespace Crismeyri_P1_AP2.BLL
+{
+    public class ProductosBLL
+    {
+        public static bool Existe(int id) // determina si existe un producto
+        {
+            Contexto db = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = db.Productos.Any(P => P.ProductoId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return encontrado;
+        }
+        public static bool Insertar ( Productos productos)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Productos.Add(productos);
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static Modificar (Productos productos)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Entry(productos).State = EntityState.Modified;
+                paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Guardar(Productos productos)
+        {
+            if (!Existe(productos.ProductoId))
+                return Insertar(productos);
+            else
+                return Modificar(productos);
+        }
+
+        public static bool Eliminar (int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+            
+            try
+            {
+                var producto = db.Productos.Find(id);
+
+                if (producto != null)
+                {
+                    db.Productos.Remove(producto); //remueve la entidad
+                    paso = db.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+
+        }
+        public static Productos Buscar(int id)
+        {
+            Contexto DB = new Contexto();
+            Productos productos;
+
+            try
+            {
+                productos = db.Productos.Find(id);
+            }
+            catch  (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                DB.Dispose();
+            }
+            return productos;
+        }
+        public static List<Productos> GetPersonas()
+        {
+            Contexto db = new Contexto();
+            try
+            {
+                lista = db.Productos.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return lista;
+        }
+        public static List<Productos> GetList(Expression<Func<Productos, bool>> criterio)
+        {
+
+            List<Productos> lista = new List<Productos>();
+            Contexto db = new Contexto(); 
+            try
+            {
+                lista = db.Productos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return lista;
+            
+        }
+    }
+}
--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-//using Crismeyri_P1_AP2.Models;
+using Crismeyri_P1_AP2.Models;
 
 namespace Crismeyri_P1_AP2.DAL
 {
     public class Contexto : DbContext
     {
        
-
+        public DbSet<Productos> Productos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-           
+            optionsBuilder.UseSqlite(@"Data Source = Data/Productos.db");
         }
     }
 
